5f7552ef47bcd32bd015a20b5853556b28749126
Paulus Esterhazy <pesterhazy@gmail.com>
2018-06-11 18:27:17 +0200

    WIP

--- src/playground/core.cljs
+++ src/playground/core.cljs
@@ -1,22 +1,45 @@
 (ns ^:figwheel-hooks playground.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [clojure.pprint :refer [pprint]]
+            [datascript.core :as d]))
+
+(defonce !conn (d/create-conn))
 
 (defn input-ui []
   [:input.form-control {:type "text"
                         :id "test"
                         :placeholder "Your name..."}])
 
+(defn add-item []
+  (d/transact! !conn [{:db/id -1
+                       :todo/caption "hello"}]))
+
+(defn transmogrify [xs]
+  (map (partial vec) xs))
+
+(defn db-viewer-ui []
+  (let [!counter (r/atom 0)]
+    (d/listen! !conn (fn []
+                       (swap! !counter inc)))
+    (fn []
+      (prn @!counter)
+      [:pre (-> @!conn (d/datoms :eavt) transmogrify vec pprint with-out-str)])))
+
 (defn root-ui []
   [:div.p-5.my-form.bg-white
    [:div
     [:h3 "Reagent playground"]
     [:p "Type into this input box"]
-    [:div.form-box
-     [:form
-      [:fieldset.form-group
-       [:label {:for "test"} "Some input"]
-       [input-ui]]]]]])
-
+    [:h4 "op log"]
+    [db-viewer-ui]
+    [:button {:on-click add-item} "add item"]
+    #_[:div.form-box
+       [:form
+        [:fieldset.form-group
+         [:label {:for "test"} "Operation:"]
+         [:input.form-control {:type "text"
+                               :id "test"
+                               :placeholder "op"}]]]]]])
 ;; Reagent+Figwheel boilerplate
 
 (defn reload []
