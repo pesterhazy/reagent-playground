dc9f24a57fd72f6bc6321d5f0e96e7aea6cecd9c
Paulus Esterhazy <pesterhazy@gmail.com>
2018-06-21 16:46:27 +0200

    WIP

--- src/playground/core.cljs
+++ src/playground/core.cljs
@@ -3,24 +3,34 @@
             [clojure.pprint :refer [pprint]]
             [com.stuartsierra.mapgraph :as mg]))
 
-(def empty-ds (-> (mg/new-db)
-                  (mg/add-id-attr :slide/id :document/id)))
+(def the-id-attrs #{:slide/id :document/id})
 
-(defn apply-op [ds [action payload]]
-  (case action
-    :db/add
-    (mg/add ds payload)))
+(def empty-ds (apply mg/add-id-attr (mg/new-db) the-id-attrs))
+
+(defn apply-op [ds [action payload lamport-ts :as op]]
+  (assert (= :db/add action))
+  (let [id-attr (->> payload keys (filter the-id-attrs) first)]
+    (assert (some? id-attr))
+    (let [lookup-ref [id-attr (get payload id-attr)]
+          entity (get ds lookup-ref)
+          existing-ts (:db/version entity)]
+      (if (and existing-ts (not= -1 (compare existing-ts lamport-ts)))
+        (do
+          (println "Skipping outdated:" op)
+          ds)
+        (mg/add ds (assoc payload :db/version lamport-ts))))))
+
+(def the-client-id 12345)
+(def the-lamport-ts [1 the-client-id])
 
 (def the-root [:document/id 1])
 
 (def the-ops
-  [[:db/add {:document/id 1}]
-   [:db/add {:slide/id 1
-             :slide/title "One"}]
-   [:db/add {:slide/id 2
-             :slide/title "Two"}]
-   [:db/add {:document/id 1
-             :document/slides [{:slide/id 1} {:slide/id 2}]}]])
+  [[:db/add {:document/id 1} [0 the-client-id]]
+   [:db/add {:slide/id 1, :slide/title "One"} [0 the-client-id]]
+   [:db/add {:slide/id 2, :slide/title "Two"} [0 the-client-id]]
+   [:db/add {:slide/id 1, :slide/title "Eins"} [0 the-client-id]]
+   [:db/add {:document/id 1 :document/slides [{:slide/id 1} {:slide/id 2}]} [1 the-client-id]]])
 
 (def the-ds (reduce apply-op empty-ds the-ops))
 
